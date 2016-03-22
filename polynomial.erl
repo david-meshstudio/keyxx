59b83ab4767285072cd8afe1eb64d086173393c5
z_k_w <z_k_w@126.com>
2016-03-23 00:35:26 +0800

    polynomial finished

--- polynomial.erl
+++ polynomial.erl
@@ -1,23 +1,28 @@
 -module(polynomial).
--export([add/2,multi/2,powlist/2,sum/1]).
+-export([sum/1,add/2,multi/2,subtract/2,mv_add/4,mv_subtract/4,mv_multi/5,remove_zerotail/1,bv_add/2,bv_subtract/2,bv_multi/2]).
+
+sum([H|T]) -> H + sum(T);
+sum([]) -> 0.
 
 add([], []) -> [];
 add([], [H|T]) -> [H|add([], T)];
 add([H|T], []) -> [H|add(T, [])];
 add([H1|T1], [H2|T2]) -> [H1 + H2|add(T1, T2)].
 
+subtract(L1, L2) -> add(L1, multi2(-1, L2)).
+
 multi(L1, L2) -> multi1(L1, L2).
 
 multi1([], _) -> [];
-multi1([H|T], L) -> add(multi2(H, L),multi1(T, [0|L])).
+multi1([H|T], L) -> add(multi2(H, L), multi1(T, [0|L])).
 
 multi2(_, []) -> [];
 multi2(H1, [H2|T2]) -> [H1 * H2|multi2(H1, T2)].
 
-% nmulti(N, L1, L2) -> 
+fullpowlist(N, M) -> fullpowlisti(N, 0, M).
 
-change(L, [H|T]) -> [{H,L}|change(nextpow(2, L), T)];
-change(_, []) -> [].
+fullpowlisti(N, M, M) -> powlist(N, M);
+fullpowlisti(N, I, M) -> powlist(N, I) ++ fullpowlisti(N, I+1, M).
 
 powlist(N, M) ->
 	L0 = initpow(N, M),
@@ -26,10 +31,8 @@ powlist(N, M) ->
 addpowlist(M, L) ->
 	Ln = nextpow(M, L),
 	case Ln == L of
-		false ->
-			[Ln|addpowlist(M, Ln)];
-		true ->
-			[]
+		false -> [Ln|addpowlist(M, Ln)];
+		true -> []
 	end.
 	
 nextpow(M, [H|T]) when H == M -> [H|T];
@@ -43,5 +46,59 @@ initpow(N, M) -> [0|initpow(N - 1, M)].
 reinpow([_|T], M) when T == [] -> [M];
 reinpow([_|T], M) -> [0|reinpow(T, M)].
 
-sum([H|T]) -> H + sum(T);
-sum([]) -> 0.
+% Multi Variable
+
+change(L, N, M) -> appendpow(L, fullpowlist(N, M)).
+
+appendpow([H|T], [HP|TP]) -> [{H,HP}|appendpow(T, TP)];
+appendpow(_, []) -> [];
+appendpow([], [HP|TP]) -> [{0,HP}|appendpow([], TP)].
+
+add_withpower([], []) -> [];
+add_withpower([{H1, HP1}|T1], [{H2, _}|T2]) -> [{H1 + H2, HP1}|add_withpower(T1, T2)].
+
+mv_add(L1, L2, N, M) -> add_withpower(change(L1, N, M), change(L2, N, M)).
+mv_subtract(L1, L2, N, M) -> mv_add(L1, multi2(-1, L2), N, M).
+
+multi_withpower_single({_, _}, []) -> [];
+multi_withpower_single({H1, HP1}, [{H2, HP2}|T]) -> [{H1 * H2, add(HP1, HP2)}|multi_withpower_single({H1, HP1}, T)].
+
+multi_withpower([], _) -> [];
+multi_withpower([{H1, HP1}|T1], L2) -> multi_withpower_single({H1, HP1}, L2) ++ multi_withpower(T1, L2).
+
+merge_bypower_single({H1, HP1}, L, []) -> [[{H1, HP1}], L];
+merge_bypower_single({H1, HP1}, L, [{H2, HP2}|T]) when HP1 == HP2 -> merge_bypower_single({H1 + H2, HP1}, L, T);
+merge_bypower_single({H1, HP1}, L, [{H2, HP2}|T]) -> merge_bypower_single({H1, HP1}, L ++ [{H2, HP2}], T).
+
+merge_bypower([{H, HP}|T]) ->
+	[L1, L2] = merge_bypower_single({H, HP}, [], T),
+	L1 ++ merge_bypower(L2);
+merge_bypower([]) -> [].
+
+mv_multi(L1, L2, N, M1, M2) -> merge_bypower(multi_withpower(change(L1, N, M1), change(L2, N, M2))).
+
+get_zeropart(L1, L2, {H, HP}, [{H2, HP2}|T]) when H == 0 -> get_zeropart(L1, L2 ++ [{H, HP}], {H2, HP2}, T);
+get_zeropart(L1, L2, {H, HP}, [{H2, HP2}|T]) -> get_zeropart(L1 ++ L2 ++ [{H, HP}], [], {H2, HP2}, T);
+get_zeropart(L1, L2, {H, HP}, []) when H == 0 -> [L1, L2 ++ [{H, HP}]];
+get_zeropart(L1, L2, {H, HP}, []) -> [L1 ++ L2 ++ [{H, HP}], []].
+
+remove_zerotail([{H, HP}|T]) ->
+	[L1, _] = get_zeropart([], [], {H, HP}, T),
+	L1.
+
+stepsum(N, N) -> N;
+stepsum(I, N) -> I + stepsum(I + 1, N).
+
+acculatesum(N) -> stepsum(1, N + 1).
+
+loop_find_maxpower(I, Len) ->
+	case acculatesum(I) >= Len of
+		true -> I;
+		false -> loop_find_maxpower(I + 1, Len)
+	end.
+
+get_maxpower(L) -> loop_find_maxpower(0, length(L)).
+
+bv_add(L1, L2) -> mv_add(L1, L2, 2, max(get_maxpower(L1), get_maxpower(L2))).
+bv_subtract(L1, L2) -> bv_add(L1, multi2(-1, L2)).
+bv_multi(L1, L2) -> mv_multi(L1, L2, 2, get_maxpower(L1), get_maxpower(L2)).
